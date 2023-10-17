118484e5d9fae3b01d10817b1f22402432814486
DorregoBraian <rayan_22_dorrego@hotmail.com>
2023-10-17 20:32:22 -0300

    Error Css 3

--- Js/components.js
+++ Js/components.js
@@ -77,19 +77,19 @@ export const Inicio = () => { //html
     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="/Img/carrusel 1.webp" class="d-block w-100" alt="...">
+                    <img src="./Img/carrusel 1.webp" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="/Img/carrusel 3.jpg" class="d-block w-100" alt="...">
+                    <img src="./Img/carrusel 3.jpg" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="/Img/carrusel 4.jpg" class="d-block w-100" alt="...">
+                    <img src="./Img/carrusel 4.jpg" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="/Img/carrusel 5.jpg" class="d-block w-100" alt="...">
+                    <img src="./Img/carrusel 5.jpg" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="/Img/carrusel 6.png" class="d-block w-100" alt="...">
+                    <img src="./Img/carrusel 6.png" class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
